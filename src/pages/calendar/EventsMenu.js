ac7cb6b713fb5a8c2672b6f2edebc6699e773902
Lindani Mabaso <LindaniM@synthesis.co.za>
2023-11-18 10:11:37 +0200

    add another page

--- src/pages/calendar/EventsMenu.js
+++ src/pages/calendar/EventsMenu.js
@@ -3,23 +3,22 @@ import { List, ListItem, ListItemButton, Box, ListItemIcon, Divider, ListItemTex
 import Iconify from '../../components/Iconify';
 
 export default function EventCalendar() {
-    const getIcon = (name) => <Iconify icon='eva:calendar-outline' fontSize="xxx-large" />;
 
-  return (
+    return (
     <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <nav aria-label="main mailbox folders">
         <List>
           <ListItem disablePadding>
             <ListItemButton>
-              <ListItemIcon icon={getIcon} >
-                <Box icon={getIcon} />
+              <ListItemIcon >
+                <Iconify icon='eva:calendar-outline' fontSize="xx-large" />
               </ListItemIcon>
               <ListItemText primary="Annual 2024 Calendar" />
             </ListItemButton>
           </ListItem>
         </List>
       </nav>
-      <Divider />
+      <Divider variant="fullWidth"/>
       <nav aria-label="secondary mailbox folders">
         <List>
           <ListItem disablePadding>
